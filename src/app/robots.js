cf9e9864bbd0d6fec43e38652ff2220e01406a26
smilebotstore <ttes61665@gmail.com>
2026-07-21 10:40:12 +0800

    Add robots.js for SEO configuration

---
+++ src/app/robots.js
@@ -0,0 +1,11 @@
+const siteUrl = "https://nasigorengjayabintang.my.id";
+
+export default function robots() {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
+    sitemap: `${siteUrl}/sitemap.xml`,
+  };
+}
