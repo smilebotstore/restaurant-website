8270bceeb0d730bf7cfbf988a2f55f3e7a6cc47f
smilebotstore <ttes61665@gmail.com>
2026-07-23 13:24:23 +0800

    Add files via upload

--- src/lib/system-prompt.js
+++ src/lib/system-prompt.js
@@ -2,8 +2,33 @@ import restaurantInfo from "@/data/restaurant-info.json";
 import menu from "@/data/menu.json";
 import systemPromptData from "@/data/system-prompt.json";
 
+function getCurrentDateTimeInfo() {
+  const now = new Date();
+  const tz = "Asia/Jakarta";
+
+  const dayDate = new Intl.DateTimeFormat("id-ID", {
+    timeZone: tz,
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  }).format(now);
+
+  const time = new Intl.DateTimeFormat("id-ID", {
+    timeZone: tz,
+    hour: "2-digit",
+    minute: "2-digit",
+    hour12: false,
+  }).format(now);
+
+  return `${dayDate}, pukul ${time} WIB`;
+}
+
 export function buildSystemPrompt() {
   return `${systemPromptData.persona}
+
+WAKTU SAAT INI: ${getCurrentDateTimeInfo()}
+(Gunakan info ini kalau relevan, misal user tanya "sekarang hari apa/tanggal berapa/jam berapa", atau untuk menyapa sesuai waktu seperti "selamat pagi/siang/sore/malam". Jangan sebutkan waktu ini kalau tidak relevan dengan pertanyaan user.)
 
 TUJUAN KAMU:
 ${systemPromptData.goals.map((g) => `- ${g}`).join("\n")}
